7f5d0cb827e712f00b4b7f8da9406e07d738702d
hasan <hasanismaiel19951995@gmail.com>
2024-02-05 14:55:49 +0200

    front end setup

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,70 @@
+<div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
+    <!-- brand name and logo-->
+    <div class="sidebar-brand d-none d-md-flex">
+        {{ config('app.name') }}    
+        <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+            <use xlink:href="assets/brand/coreui.svg#full"></use>
+        </svg>
+        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
+        <use xlink:href="assets/brand/coreui.svg#signet"></use>
+        </svg>
+    </div>
+
+    <!-- nav bar items-->
+    <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <svg class="nav-icon">
+                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
+                </svg> 
+                Dashboard
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <svg class="nav-icon">
+                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user-follow"></use>
+                </svg> 
+                Users
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <svg class="nav-icon">
+                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-money"></use>
+                </svg> 
+                Clients
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <svg class="nav-icon">
+                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
+                </svg> 
+                Projects
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <svg class="nav-icon">
+                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-list-high-priority"></use>
+                </svg> 
+                Tasks
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                <svg class="nav-icon">
+                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
+                </svg> 
+                Logout
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </li>
+    </ul>
+    <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
+</div>
